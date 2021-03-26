af5fc7b5bc19fc3c213309239d635f774a2c65a5
Ali Fateh <alifateh@example.com>
2021-03-26 22:50:25 +0430

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test_git</title>
+</head>
+
+<body>
+    <section>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat iusto accusantium voluptas fugit impedit molestias! Atque, exercitationem fugiat esse ex quam maiores rerum earum voluptatum ea nesciunt debitis doloremque molestias.
+            Alias possimus ut sed magnam accusamus corrupti voluptate qui est assumenda maiores repellendus temporibus provident delectus vitae, adipisci reprehenderit explicabo numquam nemo quos. Fugiat debitis assumenda perferendis. Inventore, eligendi ab.
+            Nam eius, mollitia eos excepturi sint iure qui porro? Voluptatem iusto laboriosam modi tempore autem deserunt reprehenderit velit numquam, optio eius animi accusantium corrupti quidem suscipit distinctio, et enim culpa?
+            Exercitationem velit, facere ipsam quaerat hic nobis! Quia inventore molestias quo mollitia vitae! Aspernatur sapiente rerum natus quasi. Ex minima delectus vero voluptatum tenetur eum dignissimos blanditiis temporibus sit similique.
+            Quia laborum fugiat id asperiores delectus dolor nihil dicta amet quaerat, impedit perferendis! Vel facere quas enim, pariatur vero exercitationem et incidunt sit ab voluptate laboriosam laudantium, magnam nesciunt molestiae.
+        </p>
+        <a href="index.php">this Page ... </a>
+
+    </section>
+</body>
+
+</html>
